770a428c23a6cad1e8fdc295a078aaa5cb243aff
as0615228-wq <as0615228@gmail.com>
2025-10-29 23:00:31 +0530

    Array

---
+++ coreJava/src/array/ArraySum.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArraySum {
+	public static void main(String[]args) {
+		int arr[]= {5,7,8,9};
+		int sum=0;
+		for(int a:arr) {
+			sum+=a;
+			System.out.println(sum);
+		}
+	}
+
+}
